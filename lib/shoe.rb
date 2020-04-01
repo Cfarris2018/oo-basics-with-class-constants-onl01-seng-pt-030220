1519d01b63af8ac3d876939f17d7dc3e2bc62ddb
Camisha  Farris <cfarris@anointedflooring.com>
2020-04-01 00:51:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,16 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+  end
+  
+  def brands=(brands)
+    @brands = brands
+    BRANDS << brands
   end
 
   def cobble
