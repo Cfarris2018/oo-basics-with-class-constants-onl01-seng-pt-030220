c04ceabe81a16df097cf3e3818f7ffae5db459f0
Camisha  Farris <cfarris@anointedflooring.com>
2020-04-01 00:55:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,14 +6,11 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    
+    BRANDS << brand
+    BRANDS == BRANDS.uniq!
   end
   
-  def brands=(brands)
-    @brands = brands
-    BRANDS << brands
-  end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
